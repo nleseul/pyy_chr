13b4bdfbf6ca68073cc0e8f1c61a7646f67a3d26
nleseul <nleseul@this-life.us>
2018-09-15 00:31:01 -0400

    Add a StandardMapInterpreter to interpret map data in the most straightforward way.

--- pyy_chr/__init__.py
+++ pyy_chr/__init__.py
@@ -1,5 +1,6 @@
 from .renderer import Renderer
 from .standard_bitplane_tile_interpreter import StandardBitplaneTileInterpreter
+from .standard_map_interpreter import StandardMapInterpreter
 from .nes_palette_interpreter import NesPaletteInterpreter
 from .grayscale_palette_interpreter import GrayscalePaletteInterpreter
 from .nes_map_interpreter import NesMapInterpreter
